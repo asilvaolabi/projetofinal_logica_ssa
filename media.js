6a776bc7e89032cb72f6551ddab849c9a93a442c
geovanacord <geovana14cordeiro15@gmail.com>
2023-07-29 12:11:51 -0300

    subindo cód de média para aula pretalab

--- media.js
+++ media.js
@@ -20,21 +20,26 @@ function calcularMedia() {
 
   let mensagem = "";
   // evite mexer no código acima!
-  let media = (nota1 + nota2 + nota3 + nota4)/4;
+  let media = (nota1+nota2+nota3+nota4)/4;
+  
+  if (media == 0) {
+        mensagem = ("infelizmente você zerou a prova");
+  }
+  else if (media >= 0.1 && media <= 3.0){
+        mensagem = ("caramba, deu ruim,você obteve media " + media + " Estude mais e tente novamente");
+  }
+  else if (media >= 3.1 && media <= 5.9) {
+        mensagem = ("você obteve media " + media + " Falta pouco para a media");
 
-  if (media === 0){
-    mensagem = " Infelizmente você zerou a prova :(";
-   } else if (media <= 3){
-     mensagem = `Caramba, deu ruim, você obteve media ${media}! Estude mais e tente novamente!`;
-    } else if (media <= 5.9){
-     mensagem = `Você obteve media ${media}! Falta pouco para a média.`;
-   } else if  (media <=7){
-     mensagem = `Você está na média com ${media}`;
-   } else if  (media <= 9.9){
-     mensagem = `Notão! Sua média é ${media}!`;
-   } else {
-     mensagem = "Hoje é seu aniversário? Pq você ta de parabéns! 10 de média";
-   }
+  } else if (media >= 6.0 && media <= 7.0) {
+        mensagem = ("você esta na media " + media);
+
+  } else if (media >= 7.1 && media <= 9.9) {
+        mensagem = ("NOTAO sua media " + media);
+
+      } else {
+        mensagem = ("hoje é seu aniversario? Pq você tá de PARABÉNS! 10 de media")
+  }
 
   
   // evite mexer na linha de código abaixo!
