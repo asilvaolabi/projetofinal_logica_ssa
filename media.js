5ca1f9656a450490fb687b0a1950014e06bd717e
Tatineris <neris.tatiane@gmail.com>
2023-07-29 12:08:23 -0300

    Subindo código da média

--- media.js
+++ media.js
@@ -21,7 +21,26 @@ function calcularMedia() {
   let mensagem = "";
   // evite mexer no código acima!
   
-
+     let media = (nota1+nota2+nota3+nota4)/4;
+  
+  if (media == 0){
+      mensagem = "Infelizmente você zerou a prova";
+  }
+  else if (media >= 0.1 && media <= 3){
+      mensagem = "Caramba,deu ruim, voce obteve nota "+ media;
+  }
+  else if (media >=3.1 && media <= 5.9){
+      mensagem = "Você obteve nota " + media + " falta pouco para a media";
+  }
+  else if (media >= 6 && media <=7){
+      mensagem = "Você está na média com " + media;
+  }
+  else if (media  >=7.1 && media <=9.9){
+      mensagem = "Notão!Sua média é " + media;
+  }
+  else {
+      mensagem = "hoje é o seu aniversário? pq vc está de parabéns!";
+  }
   
   // evite mexer na linha de código abaixo!
   document.getElementById("situacaoAluno").innerHTML = mensagem;
