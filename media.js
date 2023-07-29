5d7df8393d40760baac7c8eb7417ba724cdabb60
unknown <jufaniapp@gmail.com>
2023-07-29 11:47:24 -0300

    subindo código média do aluno do curso introdução a programação da pretalab

--- media.js
+++ media.js
@@ -20,9 +20,23 @@ function calcularMedia() {
 
   let mensagem = "";
   // evite mexer no código acima!
-  
+var media = (nota1+nota2+nota3+nota4)/4
 
+if (media == 0) {
+    mensagem = "infelizmente você zerou a prova";
+} else if (media >= 0.1 && media < 3.0) {
+    mensagem = "caramba, deu ruim, você obteve media " + media + " !Estude mais e tente novamente";
+} else if (media >= 3.1 && media < 5.9) {
+    mensagem = "você obteve media " + media + " !Falta pouco para a media";
+} else if (media >= 6.0 && media < 7.0) {
+    mensagem = "você está na media " + media + " se esforce mais";
+} else if (media >= 7.1 && media < 9.9) {
+    mensagem = "notao sua media é " + media + " falta pouco";
+} else if (media == 10){
+    mensagem = "hoje é seu aiversário?Pq você tá de parabéns! 10 de média"
+}
   
   // evite mexer na linha de código abaixo!
   document.getElementById("situacaoAluno").innerHTML = mensagem;
+
 }
