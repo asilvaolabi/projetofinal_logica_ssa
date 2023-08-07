7755a6e089da4baaad2cc13aa17bea3d2b3da206
ericassilva <140272402+ericassilva@users.noreply.github.com>
2023-08-07 14:34:05 -0300

    media.js

--- media.js
+++ media.js
@@ -1,29 +1,26 @@
-/*
-  Calcule a média da estudante, em seguida, com base na nota exiba as seguintes 
-  frases usando uma estrutura de condição:
-    1. Se a nota for 0, mostre na tela: "Infelizmente você zerou a prova :( "
-    2. Se a nota for entre 0.1 e 3, mostre na tela: 
-      "Caramba, deu ruim, você obteve media {nota}! Estude mais e tente novamente!"
-    3. Se a nota for entre 3.1 e 5.9, mostre na tela: 
-      "Você obteve media {nota}! Falta pouco para a média."
-    4. Se a nota for entre 6 e 7, mostre na tela: "Você está na média com {nota}"
-    5. Se a nota for entre 7.1 e 9.9, mostre na tela: "Notão! Sua média é {nota}!"
-    6. Se a nota for 10, mostre na tela: 
-      "Hoje é seu aniversário? Pq você ta de parabéns! 10 de média"
-*/
+var nota1 = 10;
+var nota2 = 10;
+var nota3 = 10;
+var nota4 = 10;
 
-function calcularMedia() {
-  const nota1 = parseFloat(document.getElementById("nota1").value);
-  const nota2 = parseFloat(document.getElementById("nota2").value);
-  const nota3 = parseFloat(document.getElementById("nota3").value);
-  const nota4 = parseFloat(document.getElementById("nota4").value);
+var media= (nota1+nota2+nota3+nota4)/4;
 
-  let mensagem = "";
-  // evite mexer no código acima!
-
-  
-  
-  
-  // evite mexer na linha de código abaixo!
-  document.getElementById("situacaoAluno").innerHTML = mensagem;
+if(media == 0) {
+    console.log("você zerou a prova");
+}
+    else if (media >= 0.1 && media < 3){
+    console.log("caramba deu ruim voce obteve media " + media + " estude mais e tente novamente");
+}
+    else if (media >= 3.1 && media <5.9){
+    console.log("você obeteve a media " + media + "falta pouco para a media");
+}
+   
+   else if (media >= 6 && media <7){
+    console.log("voce esta na media com a nota " + media);
+}
+else if (media >= 7.1 && media <9.9 ){
+    console.log("notão sua media é " + media);
+}
+else if (media ==10){
+    console.log("hoje é seu aniversario? porque voçê esta de parabens media 10 " + media);
 }
